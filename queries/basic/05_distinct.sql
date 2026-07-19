601f37a54f6657eaa0ec59f9182573b49b1fbd54
Sudenur Bakır <sudenurbakir7@gmail.com>
2026-07-19 16:03:47 +0300

    distinct sql
    Added comments and examples demonstrating the use of DISTINCT in SQL queries.

--- queries/basic/05_distinct.sql
+++ queries/basic/05_distinct.sql
@@ -1,1 +1,22 @@
+/*
+==================================================
+Konu: DISTINCT
+Dosya: 05_distinct.sql
 
+Amaç:
+DISTINCT ifadesi, tekrar eden kayıtları kaldırarak
+benzersiz (unique) değerleri listelemek için kullanılır.
+==================================================
+*/
+
+-- Ürün kategorilerini tekrar etmeden listele
+SELECT DISTINCT category
+FROM Products;
+
+-- Sipariş durumlarını tekrar etmeden listele
+SELECT DISTINCT order_status
+FROM Orders;
+
+-- Müşterilerin kayıt olduğu farklı tarihleri listele
+SELECT DISTINCT created_at
+FROM Customers;

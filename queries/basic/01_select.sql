3afd5a441149dfcbee16a8787cb8846b5e7d6005
Sudenur Bakır <sudenurbakir7@gmail.com>
2026-07-19 15:51:26 +0300

    select sql

--- queries/basic/01_select.sql
+++ queries/basic/01_select.sql
@@ -1,1 +1,26 @@
+/*
+==================================================
+Konu: SELECT
+Dosya: 01_select.sql
 
+Amaç:
+SELECT ifadesi, bir tablodaki verileri görüntülemek için kullanılır.
+Bu dosyada SELECT komutunun temel kullanım örnekleri yer almaktadır.
+==================================================
+*/
+
+-- Customers tablosundaki tüm kayıtları listele
+SELECT *
+FROM Customers;
+
+-- Customers tablosunda sadece ad ve soyad bilgilerini listele
+SELECT first_name, last_name
+FROM Customers;
+
+-- Products tablosunda ürün adı ve fiyat bilgilerini listele
+SELECT product_name, unit_price
+FROM Products;
+
+-- Orders tablosundaki sipariş tarihlerini listele
+SELECT order_date
+FROM Orders;

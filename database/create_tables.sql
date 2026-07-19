308e5225fee0da50be7138290ed260d162d752c1
Sudenur Bakır <sudenurbakir7@gmail.com>
2026-07-19 15:33:25 +0300

    add create tables

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -16,3 +16,39 @@ CREATE TABLE Customers (
     phone VARCHAR(20),
     created_at DATE
 );
+
+-- Products Tablosu
+
+CREATE TABLE Products (
+    product_id INT PRIMARY KEY,
+    product_name VARCHAR(100),
+    category VARCHAR(50),
+    unit_price DECIMAL(10,2),
+    stock_quantity INT,
+    created_at DATE
+);
+
+-- Orders Tablosu
+
+CREATE TABLE Orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    order_status VARCHAR(30),
+    total_amount DECIMAL(10,2),
+
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+-- Order_Items Tablosu
+
+CREATE TABLE Order_Items (
+    order_item_id INT PRIMARY KEY,
+    order_id INT,
+    product_id INT,
+    quantity INT,
+    unit_price DECIMAL(10,2),
+
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (product_id) REFERENCES Products(product_id)
+);

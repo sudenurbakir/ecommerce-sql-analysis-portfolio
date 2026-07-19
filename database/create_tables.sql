adb8a29d4887c995540144bc82e43ed302af6fd8
Sudenur Bakır <sudenurbakir7@gmail.com>
2026-07-19 15:25:13 +0300

    create tables add

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -1,1 +1,18 @@
+/*
+==================================================
+Proje: E-Ticaret SQL Analiz Portfolyosu
+Dosya: create_tables.sql
+Açıklama: E-ticaret veritabanına ait temel tabloların oluşturulması.
+==================================================
+*/
 
+-- Customers Tablosu
+
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(100),
+    phone VARCHAR(20),
+    created_at DATE
+);

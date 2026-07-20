cde823b13e2be5384397b997ee938c36aefa7c8e
Sudenur Bakır <sudenurbakir7@gmail.com>
2026-07-20 20:06:49 +0300

    window functions sql

--- queries/intermediate/08_window_functions.sql
+++ queries/intermediate/08_window_functions.sql
@@ -1,1 +1,26 @@
+/*
+==================================================
+Konu: Window Functions
+Dosya: 08_window_functions.sql
 
+Amaç:
+Window Functions, satırları gruplandırmadan
+analiz yapmayı sağlar. Özellikle sıralama,
+numaralandırma ve karşılaştırma işlemlerinde
+yaygın olarak kullanılır.
+==================================================
+*/
+
+-- Ürünleri fiyata göre sıralayarak numaralandır
+SELECT
+    product_name,
+    price,
+    ROW_NUMBER() OVER (ORDER BY price DESC) AS Sıra_No
+FROM Products;
+
+-- Ürünleri fiyata göre sıralayarak derece ver
+SELECT
+    product_name,
+    price,
+    RANK() OVER (ORDER BY price DESC) AS Fiyat_Sırası
+FROM Products;

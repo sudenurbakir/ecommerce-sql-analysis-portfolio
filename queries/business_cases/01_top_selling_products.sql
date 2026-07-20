ff2f6c98048ba6b5ae08b9a006bb8118c1aef529
Sudenur Bakır <sudenurbakir7@gmail.com>
2026-07-20 20:09:30 +0300

    top selling sql

--- queries/business_cases/01_top_selling_products.sql
+++ queries/business_cases/01_top_selling_products.sql
@@ -1,1 +1,19 @@
+/*
+==================================================
+Konu: En Çok Satan Ürünler
+Dosya: 01_top_selling_products.sql
 
+Amaç:
+Satılan ürünlerin toplam satış miktarını
+hesaplayarak en çok satılan ürünleri listelemek.
+==================================================
+*/
+
+SELECT
+    Products.product_name,
+    SUM(Order_Items.quantity) AS Total_Sales
+FROM Order_Items
+INNER JOIN Products
+ON Order_Items.product_id = Products.product_id
+GROUP BY Products.product_name
+ORDER BY Total_Sales DESC;

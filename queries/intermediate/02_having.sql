fd96ca39fa15e3ae4ba7acfd5b5e89233f1ac103
Sudenur Bakır <sudenurbakir7@gmail.com>
2026-07-20 08:05:25 +0300

    having sql

--- queries/intermediate/02_having.sql
+++ queries/intermediate/02_having.sql
@@ -1,1 +1,34 @@
+/*
+==================================================
+Konu: HAVING
+Dosya: 02_having.sql
 
+Amaç:
+HAVING ifadesi, GROUP BY ile oluşturulan grupları
+belirli koşullara göre filtrelemek için kullanılır.
+==================================================
+*/
+
+-- İkiden fazla ürüne sahip kategorileri listele
+SELECT
+    category,
+    COUNT(*) AS Ürün_Sayısı
+FROM Products
+GROUP BY category
+HAVING COUNT(*) > 2;
+
+-- Birden fazla siparişi bulunan müşterileri listele
+SELECT
+    customer_id,
+    COUNT(*) AS Sipariş_Sayısı
+FROM Orders
+GROUP BY customer_id
+HAVING COUNT(*) > 1;
+
+-- Birden fazla siparişe sahip sipariş durumlarını listele
+SELECT
+    order_status,
+    COUNT(*) AS Sipariş_Sayısı
+FROM Orders
+GROUP BY order_status
+HAVING COUNT(*) > 1;

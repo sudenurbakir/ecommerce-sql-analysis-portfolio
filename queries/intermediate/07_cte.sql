f4c1ccb567dfbb528e9754821ba04c11d7878cfe
Sudenur Bakır <sudenurbakir7@gmail.com>
2026-07-20 20:04:26 +0300

    cte sql

--- queries/intermediate/07_cte.sql
+++ queries/intermediate/07_cte.sql
@@ -1,1 +1,30 @@
+/*
+==================================================
+Konu: Common Table Expression (CTE)
+Dosya: 07_cte.sql
 
+Amaç:
+CTE, geçici bir sonuç kümesi oluşturmak için kullanılır.
+Özellikle uzun ve karmaşık sorguları daha okunabilir
+hale getirmek amacıyla tercih edilir.
+==================================================
+*/
+
+-- Ortalama fiyatın üzerindeki ürünleri CTE kullanarak listele
+WITH ProductPrices AS
+(
+    SELECT
+        product_name,
+        price
+    FROM Products
+)
+
+SELECT
+    product_name,
+    price
+FROM ProductPrices
+WHERE price >
+(
+    SELECT AVG(price)
+    FROM ProductPrices
+);
